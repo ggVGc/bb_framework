711e418286fcea244104200b21308067446fb609
valter sundstrom <valter.sundstrom@gmail.com>
2011-02-23 23:46:28 +0100

    stuff

--- project_linux/zip_assets.py
+++ project_linux/zip_assets.py
@@ -2,7 +2,7 @@ import os
 
 curDir = os.getcwd()
 os.chdir("../jumpz/")
-os.system("zip -r  ../project_linux/assets.zip assets")
+os.system("zip -ur  ../project_linux/assets.zip assets")
 os.chdir("../framework/src/lua")
-os.system("zip -r ../../../project_linux/assets.zip assets")
+os.system("zip -ur ../../../project_linux/assets.zip assets")
 os.chdir(curDir)
