8cd0421b42782a25db7dea4917887fb7ae321a0c
valter sundstrom <valter.sundstrom@gmail.com>
2011-02-25 06:59:15 +0100

    rewrite zip script

--- project_linux/zip_assets.py
+++ project_linux/zip_assets.py
@@ -1,8 +1,17 @@
 import os
+import zipfile
 
-curDir = os.getcwd()
-os.chdir("../jumpz/")
-os.system("zip -ur  ../project_linux/assets.zip assets")
-os.chdir("../framework/src/lua")
-os.system("zip -ur ../../../project_linux/assets.zip assets")
-os.chdir(curDir)
+def zipDir(out, r, pref=""):
+  for root, dirs, files in os.walk(r):
+    for f in files:
+      p = os.path.join(root, f)
+      rp = os.path.relpath(p, r)
+      if not rp.startswith("."):
+        print rp, p
+        out.write(p, os.path.join("assets", pref, rp))
+
+
+outFile = zipfile.ZipFile("assets.zip", "w")
+zipDir(outFile, "../framework/src/lua", "framework")
+zipDir(outFile, "../../bounce")
+outFile.close() 
