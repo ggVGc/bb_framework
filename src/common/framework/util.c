d723d63766c4cc8d949b58ee5b0b234432359cd5
valter sundstrom <valter.sundstrom@gmail.com>
2011-04-04 02:19:59 +0200

    Fixed hgignore

---
+++ src/common/framework/util.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void trace(const char* msg)
+{
+  if(msg != 0)
+    printf("%s\n", msg);
+  else
+    printf("%s\n", "null");
+}
+
+void traceInt(int v)
+{
+  printf("%i\n", v);
+}
