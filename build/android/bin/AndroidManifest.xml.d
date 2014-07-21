c3262d75b3e6f79521f778c9d741be374588c409
walt <devnull@localhost>
2014-07-21 17:06:53 +0200

    .asdsad

--- build/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/media/data/vmshare/bitbucket/framework/build/android/bin/AndroidManifest.xml : \
-/media/data/vmshare/bitbucket/framework/build/android/AndroidManifest.xml \
--- build/android/bin/FrameworkTest-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-/media/data/vmshare/bitbucket/framework/build/android/bin/FrameworkTest-debug-unaligned.apk : \
-/media/data/vmshare/bitbucket/framework/build/android/bin/FrameworkTest.ap_ \
-/media/data/vmshare/bitbucket/framework/build/android/bin/classes.dex \
-/media/data/vmshare/bitbucket/framework/build/android/libs/armeabi/gdbserver \
-/media/data/vmshare/bitbucket/framework/build/android/libs/armeabi/libjumpz_framework.so \
