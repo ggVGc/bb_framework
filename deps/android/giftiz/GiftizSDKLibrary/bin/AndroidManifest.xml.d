0c950ea96bfb893952da160cc3d9ae538eace634
walt <devnull@localhost>
2014-12-25 00:40:01 +0100

    asdf

--- deps/android/giftiz/GiftizSDKLibrary/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/media/data/vmshare/bitbucket/framework/deps/android/giftiz/GiftizSDKLibrary/bin/AndroidManifest.xml : \
-/media/data/vmshare/bitbucket/framework/deps/android/giftiz/GiftizSDKLibrary/AndroidManifest.xml \
